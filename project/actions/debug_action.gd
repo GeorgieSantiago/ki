49762092c107d67665cedad168d471b4135a2cb8
GeorgieSantiago <milestoneartisan@gmail.com>
2023-07-23 02:55:48 -0400

    feat: Add color option to debug message action

--- project/actions/debug_action.gd
+++ project/actions/debug_action.gd
@@ -3,7 +3,8 @@ extends Action
 class_name DebugAction
 
 @export var message: String
+@export var color: Color
 
 func _execute(_invoker: Variant) -> int:
-	print(message)
+	print_rich("[color=" + color.to_html() + "] " + message)
 	return 0
